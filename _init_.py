290b7d849d502611fc2ea19f1d15ccb1f23f9a5a
Waleed <akwaleed728@gmail.com>
2021-06-27 14:09:11 +0500

    Update Code

--- _init_.py
+++ _init_.py
@@ -50,25 +50,20 @@ def removeImageBackground(image) :
     # load image
     img = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], image))
     # convert to graky
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # threshold input image as mask
-    mask = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)[1]
-    # negate mask
-    mask = 255 - mask
-    # apply morphology to remove isolated extraneous noise
-    # use borderconstant of black since foreground touches the edges
-    kernel = np.ones((3,3), np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    # anti-alias the mask -- blur then stretch
-    # blur alpha channel
-    mask = cv2.GaussianBlur(mask, (0,0), sigmaX=2, sigmaY=2, borderType = cv2.BORDER_DEFAULT)
-    # linear stretch so that 127.5 goes to 0, but 255 stays 255
-    mask = (2*(mask.astype(np.float32))-255.0).clip(0,255).astype(np.uint8)
-    # put mask into alpha channel
-    result = img.copy()
-    result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
-    result[:, :, 3] = mask
+    hh, ww = img.shape[:2]
+    # threshold on white
+    # Define lower and uppper limits
+    lower = np.array([200, 200, 200])
+    upper = np.array([255, 255, 255])
+    # Create mask to only select black
+    thresh = cv2.inRange(img, lower, upper)
+    # apply morphology
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
+    morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    # invert morp image
+    mask = 255 - morph
+    # apply mask to image
+    result = cv2.bitwise_and(img, img, mask=mask)
     # save resulting masked image
     cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], 'trans_'+image), result)
     # display result, though it won't show transparency
